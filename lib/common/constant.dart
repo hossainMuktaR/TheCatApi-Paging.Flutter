dab707afadf1641de742b4c8b5cdba17a32e0be1
Hossain Muktar <HossainMuktarDev@gmail.com>
2024-10-12 01:46:28 +0600

    added andriod internet permission

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -1,4 +1,4 @@
 class Constant {
   Constant._();
-  static const String apikey = "live_dYoOVK3ju1CCdZhrj129Uov22imb8yM1CFqyAsRIov1xNiNQPyOIET4oh8qSfT2e"; 
+  static const String apikey = "YOUR-API-KEY"; 
 }
